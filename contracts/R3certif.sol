ece1357afd90d5de3759ceacfa1d2ce33106a5d7
Camille Gautier <camille.gaut@sfr.fr>
2023-11-14 15:09:18 +0100

    [FEAT] [CGA] first config

---
+++ contracts/R3certif.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract R3certif is ERC721 {
+   constructor() ERC721("r3certif", "R3C") {}
+   
+   
+}
